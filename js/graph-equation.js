ecff64258f11d8c2f030884ca49f7b5a54af12bc
nate-lawrence <lawrence.nd@gmail.com>
2021-04-07 10:22:52 +1000

    Added dode documentation

--- js/graph-equation.js
+++ js/graph-equation.js
@@ -1,11 +1,12 @@
-
+// Paramaters to define the graph - Manually entering this paramater data for development purposes
+// TODO: receive paramater values from user input in the UI
 const graphParams = [{
     svgGroup: 'svg-group-1',
     groupParams: {
         graphName: 'graph-0',
         funcParams: {
-            a: [-1,2],
-            b: [5],
+            a: [1,2],
+            b: [0],
         },
         plotParams: {
             interval: 100,
@@ -18,10 +19,10 @@ const graphParams = [{
         },
         axis: {
             gAxisDims: {
-                x0: 30,
-                x1: 230,
-                y0: 30,
-                y1: 230,
+                x0: 20,
+                x1: 250,
+                y0: 20,
+                y1: 250,
             },
             axesRange: {
                 xAxisRange: [-5, 5],
@@ -34,11 +35,14 @@ const graphParams = [{
         },            
     },
 }];
-const graphGen = (graphParams) => {
 
+// Generate separate elements forming the graph - the plotline, and the axis
+const graphGen = (graphParams) => {
+    // These variables are common across multiple element generators
     const xAxisRange = graphParams.axis.axesRange.xAxisRange;
     const yAxisRange = graphParams.axis.axesRange.yAxisRange;
-
+    // This function generates the 2d representation of f(x) on the graph, the dimensions of the graph
+    // as defined in the 'graphParams'
     const plotFunction = (graphParams, xAxisRange, yAxisRange) => {
         const xAxisMax = Math.max(...xAxisRange);
         const xAxisMin = Math.min(...xAxisRange);
@@ -48,35 +52,32 @@ const graphGen = (graphParams) => {
         const funcParams = graphParams.funcParams;
         const xPxRatio = (plotDims.x1 - plotDims.x0) / (xAxisMax - xAxisMin);
         const yPxRatio = (plotDims.y1 - plotDims.y0) / (yAxisMax - yAxisMin);
-
+        // A standard range generating function, outputs a new array of elements, each i incremented by one 'step',
+        // and bound by the specified min and max values
         const range = (start, stop, step) => {
             const rangeArray = Array.from({length: ( stop - start ) / step}, (_, i) => start + (i * step) );
             return rangeArray;
-        };
-
+        };        
         const xPlotPoints = range(xAxisMin, xAxisMax, (xAxisMax - xAxisMin) / graphParams.plotParams.interval);
+        // Converts each plot point on each axis as specified by the scale, to the corresponding point in pixels within the SVG div
+        // The Y value is calculated as a function of x using the linear/polynomial function
+        // TODO: Allow for n factor polynomials by iterating over values
         const plotPointsToDisplay = xPlotPoints.reduce( (acc, ele) => {
-            // Converts each point on each axis as specified by the scale, to the corresponding point in pixels within the SVG div
-            // The Y value is calculated as a function of x using the linear/polynomial function
-            // TODO: Allow for n factor polynomials by iterating over values
             const yPlotPoint = plotDims.y1 - (((( funcParams.a[0] * Math.pow( ele, funcParams.a[1] ) + funcParams.b[0] ) - yAxisMin) * yPxRatio) /*+ plotDims.y0*/);
             const xPlotPoint = (( ele - xAxisMin) * xPxRatio) + plotDims.x0;
             return ( [...acc, [xPlotPoint, yPlotPoint]] )
         }, [] );
-
+        // Converts the coordinates to syntax that can be parsed by the 'path' element
         const plotLine = plotPointsToDisplay.map( (e, i, a) => {
             return i === 0
                 ? `M ${e[0]} ${e[1]} `
                 : `L ${e[0]} ${e[1]} `
         });
-
         return plotLine;
     };        
-
+    // Converts location of each axis line as specified by the scale, to the corresponding point in pixels wihtin the SVG div
     const axisPointDimRange = (xPoint, yPoint, graphParams, xAxisRange, yAxisRange) => {
-        //const groupDims = graphParams.groupDims;
         const axisDims = graphParams.axis.gAxisDims;
-
         const xAxisDimInitPoints = [axisDims.x0, axisDims.y1 - ((xPoint - Math.min(...yAxisRange)) / (Math.abs(yAxisRange[0]) + Math.abs(yAxisRange[1])) * (axisDims.y1 - axisDims.y0))];
         const yAxisDimInitPoints = [(yPoint - Math.min(...xAxisRange)) / (Math.abs(xAxisRange[0]) + Math.abs(xAxisRange[1])) * (axisDims.x1 - axisDims.x0) + axisDims.x0, axisDims.y0];
         const xAxisEndPoints = [axisDims.x1, xAxisDimInitPoints[1]]
@@ -86,10 +87,12 @@ const graphGen = (graphParams) => {
     const plotLine = plotFunction(graphParams, xAxisRange, yAxisRange);
     const axisPoints = axisPointDimRange(0, 0, graphParams, xAxisRange, yAxisRange);
     return {axisPoints: axisPoints, plotLine: plotLine};
-};    
+};
+// The result of the generated graph elements, returned as an array    
 const graphElements = graphGen(graphParams[0].groupParams);
-console.log(graphElements);
 
+// Attaching the elements to the DOM and setting the attributes to display the graph in SVG
+// TODO: clean this up and wrap each step in distinct functions
 const parentNode = document.querySelector('#visualise-panel');
 const svgNode = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 svgNode.setAttribute('width', parentNode.offsetWidth);
